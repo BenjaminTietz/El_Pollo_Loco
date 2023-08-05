58b5ba69a5d5b59094b7ba65670e76cee92b28fb
Benjamin Tietz <benjamin0tietz@gmail.com>
2023-08-05 13:23:28 +0200

    implemented sound while walking

--- models/character.class.js
+++ models/character.class.js
@@ -12,6 +12,7 @@ class Character extends MovableObject {
         'img/2_character_pepe/2_walk/W-26.png'
     ];
     world;
+    walking_sound = new Audio('audio/walk.mp3');
 
     constructor () {    // constructor wird als erstes ausgeführt wenn ein Objekt neu erstellt wird
         super().loadImage('img/2_character_pepe/2_walk/W-21.png');  // durch "super." wird von der übergeorneten Klasse eine Funktion aufgerufen
@@ -23,13 +24,16 @@ class Character extends MovableObject {
     animate () {
 
         setInterval(() =>{                                  // Diese "setInterval" Funktion beinhaltet 2 if Schleifen welche erfassen ob wir die linke oder rechte Pfeiltaste gedrückt haben.
+            this.walking_sound.pause();                  // Hier wird die Variable walking_sound abgespielt an dessen der Pfad der mp3 gebunden ist.
             if (this.world.keyboard.right && this.x <this.world.level.level_end_x) {// WENN unsere rechte Pfeiltaste den Wert den Wert true hat. Duch dass && this < this.world.level.level_end_x begrenzen wir unsere Welt nach rechts und erstellen eine unsichtbare Mauer, so dass unser Character nichtmehr aus der Map herauslaufen kann.
                 this.x += this.speed;                       // Auf die X Koordinate werden 10px addiert
                 this.otherDirection = false;                // Variabel "otherDirection" bekommt den Wert = "false"
+                this.walking_sound.play();                  // Hier wird die Variable walking_sound abgespielt an dessen der Pfad der mp3 gebunden ist.
             }
             if (this.world.keyboard.left && this.x >0) {    // WENN unsere linke Pfeiltaste den Wert den Wert true hat. Duch dass && this > 0 begrenzen wir unsere Welt nach links und erstellen eine unsichtbare Mauer, so dass unser Character nichtmehr aus der Map herauslaufen kann.
                 this.x -= this.speed;                       // Von der X Koordinate werden 10px subtrahiert
                 this.otherDirection = true;                 // Variabel "otherDirection" bekommt den Wert = "true". Wenn der Wert = "true" ist soll das Bild unseres Characters gespiegelt werden.
+                this.walking_sound.play();                  // Hier wird die Variable walking_sound abgespielt an dessen der Pfad der mp3 gebunden ist.
                 }
             this.world.camera_x = -this.x + 100;            // Hier wird unsere Cameramethode an unseren Character gebunden. Jedes mal wenn wir die X-Koordinate unseres Characters verändern egal ob pos. oder neg. wird die X-Koordinate an unsere camera X-koordinate als Gegenteil gebunden. Mit den +100px verschieben wir die Kameraperspektive ein wenig nach rechts.
         }, 1000 / 60);                                      // Hier wird die Intervallgeschwindigkeit, in welcher unsere Funktion ausgeführt hat, definiert. 1000ms / 60 = 60FPS
